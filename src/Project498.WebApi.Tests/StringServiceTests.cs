84d8a5f6a2db0fff5365d72cb729e0f4c4b5d750
Iván Dombiak <idombiak@gmail.com>
2026-02-18 16:57:18 -0800

    Added ReverseWords() method which takes in a string and reverses the word ordering, while the Reverse() method reverses a strings string.

--- src/Project498.WebApi.Tests/StringServiceTests.cs
+++ src/Project498.WebApi.Tests/StringServiceTests.cs
@@ -5,6 +5,7 @@ namespace Project498.WebApi.Tests;
 public class StringServiceTests
 {
     private readonly StringService _stringService = new();
+    private readonly StringService _stringService2 = new();
 
     [Theory]
     [InlineData("hello", "olleh")]
@@ -19,6 +20,23 @@ public class StringServiceTests
     public void Reverse_WithVariousInputs_ReturnsExpectedResult(string? input, string expected)
     {
         var result = _stringService.Reverse(input!);
+
+        Assert.Equal(expected, result);
+    }
+    
+    [Theory]
+    [InlineData("hello", "hello")]
+    [InlineData("", "")]
+    [InlineData(null, "")]
+    [InlineData("a", "a")]
+    [InlineData("racecar", "racecar")]
+    [InlineData("hello world", "world hello")]
+    [InlineData("hello  world", "world  hello")]
+    [InlineData(" hello world", "world hello ")]
+    [InlineData("hello world ", " world hello")]
+    public void Reverse_WithMoreInputs_ReturnExpectedResults(string? input, string expected)
+    {
+        var result = _stringService2.ReverseWords(input!);
 
         Assert.Equal(expected, result);
     }
